5f290f4b27e36bd346bdcbc70ef29264cd168e0e
CLEITI6966 <biungreu@gmail.com>
2025-02-24 10:49:36 -0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,4 +1,4 @@
 -- Carrega e executa o script da URL 2
-local url = "https://raw.githubusercontent.com/CLEITI6966/Brookhaven/refs/heads/main/Protected_5402552525343918.txt"
+local url = "https://raw.githubusercontent.com/CLEITI6966/Brookhaven/refs/heads/main/Protected_8623130628160363.txt"
 local response = game:HttpGet(url, true)
 loadstring(response)()
